aeeea56bdf38940be26af6f35440a370c9b6b567
mschneid <michael.schneider@tu-berlin.de>
2015-09-10 11:15:36 +0200

    strange vis bug

--- src/output_matrices.py
+++ src/output_matrices.py
@@ -1,7 +1,7 @@
 
 import cPickle
 import numpy as np 
-shift_dict = cPickle.load(open( "/scratch/schneider/projects/pagerank_refinement/data/shifts_sigma_0.05.txt", "rb" ))
+shift_dict = cPickle.load(open( "shifts.p", "rb" ))
 
 #print shift_dict[("H", "H")]
 
@@ -30,12 +30,12 @@ def to_matrix(shift_dict):
 ele = ["H", "E", "C"]
 s = shift_matrix()
 print s
-"""
+
 for e in ele:
     for f in ele:
         print e, f
         mat = to_matrix(shift_dict[(e, f)])
         print mat.shape
         np.savetxt("%s_%s_matrix.txt"%(e,f), mat)
-        break
-"""
+        
+
